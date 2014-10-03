7a67ffac89c92a237ce380bdf76c195dd7df2934
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2014-10-03 14:21:31 +0100

    Added comments

--- examples/simple_forecast/simple_forecast.py
+++ examples/simple_forecast/simple_forecast.py
@@ -2,18 +2,25 @@
 
 import datapoint
 
+# Create datapoint connection
 conn = datapoint.Manager(api_key="aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee")
 
+# Get nearest site and print out its name
 site = conn.get_nearest_site(-0.124626, 51.500728)
 print site.name
 
+# Get a forecast for the nearest site
 forecast = conn.get_forecast_for_site(site.id, "3hourly")
 
+# Loop through days and print date
 for day in forecast.days:
     print "\n%s" % day.date
+
+    # Loop through time steps and print out info
     for timestep in day.timesteps:
         print timestep.name
         print timestep.weather.text
         print "%s%s%s" % (timestep.temperature.value,
                           u'\xb0', #Unicode character for degree symbol
                           timestep.temperature.units)
+
