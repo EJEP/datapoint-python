13eb3d6c13d34e13f235e5192a703a8457035ee3
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2014-10-15 12:58:28 +0100

    String int test

--- tests/unit/manager_test.py
+++ tests/unit/manager_test.py
@@ -13,9 +13,13 @@ class TestManager:
         assert type(weather_text) is StringType
 
     @raises(ValueError)
-    def test_weather_to_text_invalid_input(self):
+    def test_weather_to_text_invalid_input_None(self):
         weather_text = self.manager._weather_to_text(None)
 
     @raises(ValueError)
-    def test_weather_to_text_out_of_bounds_input(self):
+    def test_weather_to_text_invalid_input_out_of_bounds(self):
         weather_text = self.manager._weather_to_text(31)
+
+    @raises(ValueError)
+    def test_weather_to_text_invalid_input_String(self):
+        weather_text = self.manager._weather_to_text('1')
