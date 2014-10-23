a905b65d028fc3b0ed5aa4d97dac186e4a95e7fe
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2014-10-23 21:26:11 +0100

    Added postcode example

---
+++ examples/postcodes_to_lat_lng/README.md
@@ -0,0 +1,26 @@
+# Current Weather
+
+_A variation on current_weather.py which uses postcodes rather than lon lat._
+
+### Required Modules
+ * [datapoint](https://github.com/jacobtomlinson/datapoint-python)
+ * [postcodes](https://github.com/e-dard/postcodes)
+
+## Learning Objective
+
+Learn how to make use of the postcodes module to make it even simpler to access
+Met Office data.
+
+## Example Usage
+
+```Shell
+python postcodes_to_lat_lng.py
+```
+
+## Example Output
+
+```
+Horseguards Parade
+Overcast
+15°C
+```
---
+++ examples/postcodes_to_lat_lng/postcodes_to_lat_lng.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+"""
+A variation on current_weather.py which uses postcodes rather than lon lat.
+"""
+
+import datapoint
+import postcodes
+
+# Create datapoint connection
+conn = datapoint.Manager(api_key="aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee")
+
+# Get longitude and latitude from postcode
+pc = postcodes.PostCoder()
+result = pc.get('SW1A 2AA')
+latitude = result['geo']['lat']
+longitude = result['geo']['lng']
+
+# Get nearest site and print out its name
+site = conn.get_nearest_site(longitude, latitude)
+print site.name
+
+# Get a forecast for the nearest site
+forecast = conn.get_forecast_for_site(site.id, "3hourly")
+
+# Get the current timestep using now() and print out some info
+now = forecast.now()
+print now.weather.text
+print "%s%s%s" % (now.temperature.value,
+                  u'\xb0', #Unicode character for degree symbol
+                  now.temperature.units)
