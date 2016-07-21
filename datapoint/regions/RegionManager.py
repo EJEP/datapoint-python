324eac9320c4b2114acf937f3704ba9f30f4f65a
Alex Hilson <alexhilsonwork@googlemail.com>
2016-07-21 15:34:27 +0100

    Add capability to retrieve region details for #5

---
+++ datapoint/regions/RegionManager.py
@@ -0,0 +1,60 @@
+import json
+from time import time
+
+import requests
+
+from datapoint.Site import Site
+from datapoint.regions.region_names import REGION_NAMES
+
+REGIONS_BASE_URL = 'http://datapoint.metoffice.gov.uk/public/data/txt/wxfcs/regionalforecast/json'
+
+class RegionManager(object):
+    '''
+    Datapoint Manager for national and regional text forecasts 
+    '''
+    def __init__(self, api_key, base_url=None):
+        self.api_key = api_key
+        self.all_regions_path = '/sitelist'
+        if not base_url:
+            self.base_url = REGIONS_BASE_URL
+
+        # The list of regions changes infrequently so limit to requesting it
+        # every hour.
+        self.regions_last_update = 0
+        self.regions_last_request = None
+        self.regions_update_time = 3600
+
+    def call_api(self, path, **kwargs):
+        '''
+        Call datapoint api
+        '''
+        if 'key' not in kwargs:
+            kwargs['key'] = self.api_key
+        req = requests.get('{}{}'.format(self.base_url, path), params=kwargs)
+        
+        if req.status_code != requests.codes.ok:
+            req.raise_for_status()
+
+        return req.json()
+
+    def get_all_regions(self):
+        '''
+        Request a list of regions from Datapoint. Returns each Region 
+        as a Site object. Regions rarely change, so we cache the response 
+        for one hour to minimise requests to API. 
+        '''
+        if (time() - self.regions_last_update) < self.regions_update_time:
+            return self.regions_last_request
+
+        response = self.call_api(self.all_regions_path)
+        regions = []
+        for location in response['Locations']['Location']:
+            region = Site()
+            region.id = location['@id']
+            region.region = location['@name']
+            region.name = REGION_NAMES[location['@name']]
+            regions.append(region)
+
+        self.regions_last_update = time()
+        self.regions_last_request = regions
+        return regions
---
+++ tests/integration/regions_test.py
@@ -0,0 +1,40 @@
+import os
+
+from nose.tools import *
+from requests import HTTPError
+
+import datapoint
+
+
+class TestRegions(object):
+    def __init__(self):
+        self.manager = datapoint.Manager(api_key=os.environ['API_KEY'])
+        self.regions = self.manager.regions
+
+    def test_key(self):
+        assert self.regions.api_key == os.environ['API_KEY']
+
+    def test_call_api(self):
+        assert (
+            u'RegionalFcst' in self.regions.call_api('/500'))
+        assert_raises(
+            HTTPError, self.regions.call_api, '/fake_path')
+        assert_raises(
+            HTTPError, self.regions.call_api, '/500', key='fake_key')
+
+    def test_get_all_regions(self):
+        all_regions = self.regions.get_all_regions()
+        sample_region = filter(lambda x: x.id == '515', all_regions)[0]
+        assert (sample_region.name == 'UK')
+        assert (sample_region.region == 'uk')
+    
+    def test_get_forecast(self):
+        sample_region = self.regions.get_all_regions()[0]
+        response = self.regions.get_forecast(sample_region.id)
+        assert (response['regionId'] == sample_region.id)
+
+        # Datapoint currently serves
+        forecast_periods = response['FcstPeriods']['Periods']
+        forecast_ids = [period.id for period in forecast_periods]
+        expected_ids = ['day1to2', 'day3to5', 'day6to15', 'day16to30']
+        assert (forecast_ids == expected_ids)
