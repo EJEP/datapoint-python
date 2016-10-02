526513ec67f569e369f4f0d78d92b3f7ef972d92
Duncan Parkes <deparkes@gmail.com>
2016-10-02 17:40:05 +0100

    Unit test to expose forecast.now() bug

---
+++ tests/unit/forecast_test.py
@@ -0,0 +1,25 @@
+from types import *
+from nose.tools import *
+
+import datetime
+
+import datapoint
+
+
+
+class TestForecast:
+
+    def __init__(self):
+        self.forecast = datapoint.Forecast.Forecast()
+
+    def test_forecast_now_works(self):
+        test_day = datapoint.Day.Day()
+        test_day.date = datetime.datetime.utcnow()
+        
+        test_timestep = datapoint.Timestep.Timestep()
+        test_timestep.name = 1
+        
+        test_day.timesteps.append(test_timestep)        
+        
+        self.forecast.days.append(test_day)        
+        assert self.forecast.now()
