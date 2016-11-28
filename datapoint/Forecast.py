dcb97306db097476764161f4139ba442dd06d5c3
Duncan Parkes <deparkes@gmail.com>
2016-11-28 09:30:01 +0000

    Fix forecast.now() not working (#26)
    * fix forecast.now() not working
    
    self.date and datetime were in different formats so never evaluating to
    TRUE
    
    * print stuff for debugging
    
    * different return
    
    * Revert "fix forecast.now() not working"
    
    This reverts commit 015fc18764dbfd7aa657704786e2f584950e93a5.
    
    * Fix forecast.now() not working
    
    * Unit test to expose forecast.now() bug
    
    * add blank timedelta test
    
    * Remove timedelta test
    
    * Add fix for python 2.6
    
    * Fix self.timedelta_total_seconds

--- datapoint/Forecast.py
+++ datapoint/Forecast.py
@@ -1,4 +1,5 @@
 import datetime
+import sys
 
 class Forecast(object):
     def __init__(self, api_key=""):
@@ -14,14 +15,24 @@ class Forecast(object):
         self.elevation = None
         self.days = []
 
+    def timedelta_total_seconds(self, timedelta):
+        return (
+            timedelta.microseconds + 0.0 +
+            (timedelta.seconds + timedelta.days * 24 * 3600) * 10 ** 6) / 10 ** 6
+
     def now(self):
         """
         Function to return just the current timestep from this forecast
         """
         now = None
         d = datetime.datetime.utcnow()
-        msm = (d - d.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds() / 60
-        if self.days[0].date == d.strftime("%Y-%m-%dZ"):
+        for_total_seconds = d - d.replace(hour=0, minute=0, second=0, microsecond=0)
+        # python 2.6 does not have timedelta.total_seconds()
+        if sys.version_info < (2,7):
+            msm = self.timedelta_total_seconds(for_total_seconds) / 60
+        else:
+            msm = for_total_seconds.total_seconds() / 60
+        if self.days[0].date.strftime("%Y-%m-%dZ") == d.strftime("%Y-%m-%dZ"):
             for timestep in self.days[0].timesteps:
                 if timestep.name > msm:
                     break
