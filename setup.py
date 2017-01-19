92211ba284dc7715e669ac52a75dffaf1c4d609a
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2017-01-19 16:23:57 +0000

    Increment version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='datapoint',
-      version='0.4.2',
+      version='0.4.3',
       install_requires=[
           "requests >= 2.3.0",
           "appdirs",
