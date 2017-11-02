958526be1f6abd23928b105e19fdf5207f4e8f0e
Lee Graham <lee.graham@live.co.uk>
2017-11-02 23:53:55 +0000

    Added more tests

--- tests/integration/manager_test.py
+++ tests/integration/manager_test.py
@@ -104,7 +104,8 @@ class TestManager:
                     assert 0 <= int(timestep.uv.value) < 20
                     
     def test_get_nearest_observation_site(self):
-        pass
+        site = self.manager.get_nearest_site(-0.124626, 51.500728)
+        assert site.name == 'Kenley'
                     
     def test_get_observation_sites(self):
         sites = self.manager.get_observation_sites()
@@ -112,4 +113,51 @@ class TestManager:
         assert sites
         
     def test_get_observation(self):
-        pass
+	
+		site = self.manager.get_nearest_observation_site(-0.124626, 51.500728)
+		observation = self.manager.get_observations_for_site(site.id)
+		assert isinstance(observation, datapoint.Observation.Observation)
+        assert observation.continent.upper() == 'EUROPE'
+        assert observation.country.upper() == 'ENGLAND'
+        assert observation.name.upper() == 'LONDON'
+		assert abs(float(observation.longitude) - (-0.124626)) < 0.1
+        assert abs(float(observation.latitude) - 51.500728) < 0.1
+		
+		# Observation should be from within the last hour
+        tz = observation.data_date.tzinfo
+        assert (observation.data_date
+            - datetime.datetime.now(tz=tz) < datetime.timedelta(hours=1))
+			
+		# First observation should be between 24 and 25 hours old
+        tz = forecast.days[0].timesteps[0].date.tzinfo
+        assert (forecast.days[0].timesteps[0].date -
+            datetime.datetime.now(tz=tz) > datetime.timedelta(hours=24))
+		assert (forecast.days[0].timesteps[0].date -
+            datetime.datetime.now(tz=tz) < datetime.timedelta(hours=25))
+	
+	'''
+        for day in forecast.days:
+            for timestep in day.timesteps:
+                assert timestep.name in ['Day', 'Night']
+                assert self.manager._weather_to_text(
+                    int(timestep.weather.value)) == timestep.weather.text
+                assert -100 < timestep.temperature.value < 100
+                assert timestep.temperature.units == 'C'
+                assert -100 < timestep.feels_like_temperature.value < 100
+                assert timestep.feels_like_temperature.units == 'C'
+                assert 0 <= timestep.wind_speed.value < 300
+                assert timestep.wind_speed.units == 'mph'
+                for char in timestep.wind_direction.value:
+                    assert char in ['N', 'E', 'S', 'W']
+                assert timestep.wind_direction.units == 'compass'
+                assert 0 <= timestep.wind_gust.value < 300
+                assert timestep.wind_gust.units == 'mph'
+                assert (timestep.visibility.value in
+                    ['UN', 'VP', 'PO', 'MO', 'GO', 'VG', 'EX'])
+                assert 0 <= timestep.precipitation.value <= 100
+                assert timestep.precipitation.units == '%'
+                assert 0 <= timestep.humidity.value <= 100
+                assert timestep.humidity.units == '%'
+                if hasattr(timestep.uv, 'value'):
+                    assert 0 < int(timestep.uv.value) < 20
+		'''
