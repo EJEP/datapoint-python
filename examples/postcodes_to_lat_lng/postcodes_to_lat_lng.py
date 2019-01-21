882a169a8758ca643af774c79b9b31951671e760
emlyn.price <emlyn.je.price@gmail.com>
2019-01-21 21:30:10 +0000

    Change postcodes api to use postcodes.io

--- examples/postcodes_to_lat_lng/postcodes_to_lat_lng.py
+++ examples/postcodes_to_lat_lng/postcodes_to_lat_lng.py
@@ -4,16 +4,16 @@ A variation on current_weather.py which uses postcodes rather than lon lat.
 """
 
 import datapoint
-import postcodes
+import postcodes_io_api
 
 # Create datapoint connection
 conn = datapoint.Manager(api_key="aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee")
 
 # Get longitude and latitude from postcode
-pc = postcodes.PostCoder()
-result = pc.get('SW1A 2AA')
-latitude = result['geo']['lat']
-longitude = result['geo']['lng']
+postcodes_conn = postcodes_io_api.Api()
+postcode = postcodes_conn.get_postcode('SW1A 2AA')
+latitude = postcode['result']['latitude']
+longitude = postcode['result']['longitude']
 
 # Get nearest site and print out its name
 site = conn.get_nearest_site(longitude, latitude)
