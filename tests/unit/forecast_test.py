7af7c7106fc5b80d14e1b247fe9bb2d8fcdb6fb5
emlyn.price <emlyn.je.price@gmail.com>
2019-02-01 20:57:15 +0000

    Clean whitespace in forecast_test.py

--- tests/unit/forecast_test.py
+++ tests/unit/forecast_test.py
@@ -15,11 +15,11 @@ class TestForecast:
     def test_forecast_now_works(self):
         test_day = datapoint.Day.Day()
         test_day.date = datetime.datetime.utcnow()
-        
+
         test_timestep = datapoint.Timestep.Timestep()
         test_timestep.name = 1
-        
-        test_day.timesteps.append(test_timestep)        
-        
-        self.forecast.days.append(test_day)        
+
+        test_day.timesteps.append(test_timestep)
+
+        self.forecast.days.append(test_day)
         assert self.forecast.now()
