6a80aa4c30bb9fe2e1277686afe29a7bcd36fe17
emlyn.price <emlyn.je.price@gmail.com>
2019-02-25 21:05:56 +0000

    Use the full commands from imports

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 # Need to change the place we put in path to work with readthedocs
-sys.path.insert(0, join(dirname(__file__), pardir))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), os.pardir))
 import datapoint
 
 # -- Project information -----------------------------------------------------
