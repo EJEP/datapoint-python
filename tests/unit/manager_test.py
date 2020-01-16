55d1981f1311ed86b728e834b4436240e9a4606e
emlyn.price <emlyn.je.price@gmail.com>
2020-01-16 19:37:50 +0000

    Move manager_test.py to use unittest

--- tests/unit/manager_test.py
+++ tests/unit/manager_test.py
@@ -1,37 +1,29 @@
-from types import *
-from nose.tools import *
-
+import unittest
 import datapoint
 
-class TestManager:
+class ManagerTestCase(unittest.TestCase):
 
-    def __init__(self):
+    def setUp(self):
         self.manager = datapoint.Manager(api_key="")
 
     def test_weather_to_text_is_string(self):
         weather_text = self.manager._weather_to_text(0)
-        assert isinstance(weather_text, type(""))
+        self.assertIsInstance(weather_text, type(""))
 
-    @raises(ValueError)
     def test_weather_to_text_invalid_input_None(self):
-        weather_text = self.manager._weather_to_text(None)
+        self.assertRaises(ValueError, self.manager._weather_to_text, None)
 
-    @raises(ValueError)
     def test_weather_to_text_invalid_input_out_of_bounds(self):
-        weather_text = self.manager._weather_to_text(31)
+        self.assertRaises(ValueError, self.manager._weather_to_text, 31)
 
-    @raises(ValueError)
     def test_weather_to_text_invalid_input_String(self):
-        weather_text = self.manager._weather_to_text('1')
+        self.assertRaises(ValueError, self.manager._weather_to_text, '1')
 
-    @raises(ValueError)
     def test_visbility_to_text_invalid_input_None(self):
-        visibility = self.manager._weather_to_text(None)
+        self.assertRaises(ValueError, self.manager._weather_to_text, None)
 
-    @raises(ValueError)
     def test_visibility_to_text_invalid_input_out_of_bounds(self):
-        visibility = self.manager._weather_to_text(-1)
+        self.assertRaises(ValueError, self.manager._weather_to_text, -1)
 
-    @raises(ValueError)
     def test_visibility_to_text_invalid_input_String(self):
-        visibility = self.manager._weather_to_text('1')
+        self.assertRaises(ValueError, self.manager._weather_to_text, '1')
