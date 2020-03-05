1811bf056f19d680d212f2f4599453b94ca343da
Emlyn Price <emlyn.je.price@gmail.com>
2020-03-05 20:56:37 +0000

    Add tests for 'normal' times and shortly after midnight, tidy

--- tests/unit/test_forecast.py
+++ tests/unit/test_forecast.py
@@ -6,46 +6,7 @@ import unittest
 class TestForecast(unittest.TestCase):
 
     def setUp(self):
-        self.forecast = datapoint.Forecast.Forecast()
-
-    def test_forecast_now_works(self):
-        test_day_0 = datapoint.Day.Day()
-        test_day_0.date = datetime.datetime.now(datetime.timezone.utc)
-
-        test_timestep_0 = datapoint.Timestep.Timestep()
-        test_timestep_0.name = 0
-        test_timestep_0.date = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(hours=2)
-
-        test_timestep_1 = datapoint.Timestep.Timestep()
-        test_timestep_1.name = 1
-        test_timestep_1.date = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(hours=4)
-
-        test_day_0.timesteps.append(test_timestep_0)
-        test_day_0.timesteps.append(test_timestep_1)
-
-        self.forecast.days.append(test_day_0)
-
-        test_day_1 = datapoint.Day.Day()
-        for i in range(8):
-            ts = datapoint.Timestep.Timestep()
-            ts.name = i * 180
-            ts.date = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(days=1, hours=i*3)
-
-            test_day_1.timesteps.append(ts)
-        self.forecast.days.append(test_day_1)
-
-        # What is being asserted here?
-        #print(self.forecast.now())
-        assert self.forecast.now()
-
-    def test_at_datetime_1_5_hours_before_after(self):
-
-        # Generate 8 timesteps These are set at 00:00, 03:00, 06:00, 09:00,
-        # 12:00, 15:00, 18:00, 21:00 and don't got far into the past For this
-        # test we are checking the forecast for 07:00 on 2020-03-03, at 08:45
-        # on 2020-03-03 so we need timesteps from 09:00 (5 of them) and the day
-        # after Also checked is the forecast for 23:00 on 2020-03-04. These
-        # both raise APIException
+        self.forecast_3hrly = datapoint.Forecast.Forecast()
 
         test_day_0 = datapoint.Day.Day()
         test_day_0.date = datetime.datetime(2020, 3, 3, tzinfo=datetime.timezone.utc)
@@ -65,27 +26,8 @@ class TestForecast(unittest.TestCase):
             ts.date = datetime.datetime(2020, 3, 4, 3*i, tzinfo=datetime.timezone.utc)
             test_day_1.timesteps.append(ts)
 
-        forecast = datapoint.Forecast.Forecast()
-
-        forecast.days.append(test_day_0)
-        forecast.days.append(test_day_1)
-
-        target_before = datetime.datetime(2020, 3, 3, 7, 0,
-                                          tzinfo=datetime.timezone.utc)
-
-        target_after = datetime.datetime(2020, 3, 4, 23, 0,
-                                          tzinfo=datetime.timezone.utc)
-
-
-        self.assertRaises(datapoint.exceptions.APIException,
-                          forecast.at_datetime, target_before)
-
-        self.assertRaises(datapoint.exceptions.APIException,
-                          forecast.at_datetime, target_after)
-
-    def test_at_datetime_6_hours_before_after(self):
-
-        # Generate 2 timesteps These are set at 00:00 and 12:00
+        self.forecast_3hrly.days.append(test_day_0)
+        self.forecast_3hrly.days.append(test_day_1)
 
         test_day_0 = datapoint.Day.Day()
         test_day_0.date = datetime.datetime(2020, 3, 3, tzinfo=datetime.timezone.utc)
@@ -105,10 +47,60 @@ class TestForecast(unittest.TestCase):
             ts.date = datetime.datetime(2020, 3, 4, 2*i, tzinfo=datetime.timezone.utc)
             test_day_1.timesteps.append(ts)
 
+        self.forecast_daily = datapoint.Forecast.Forecast()
+
+        self.forecast_daily.days.append(test_day_0)
+        self.forecast_daily.days.append(test_day_1)
+
+
+    def test_forecast_now_works(self):
         forecast = datapoint.Forecast.Forecast()
+        test_day_0 = datapoint.Day.Day()
+        test_day_0.date = datetime.datetime.now(datetime.timezone.utc)
+
+        test_timestep_0 = datapoint.Timestep.Timestep()
+        test_timestep_0.name = 0
+        test_timestep_0.date = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(hours=2)
+
+        test_timestep_1 = datapoint.Timestep.Timestep()
+        test_timestep_1.name = 1
+        test_timestep_1.date = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(hours=4)
+
+        test_day_0.timesteps.append(test_timestep_0)
+        test_day_0.timesteps.append(test_timestep_1)
 
         forecast.days.append(test_day_0)
+
+        test_day_1 = datapoint.Day.Day()
+        for i in range(8):
+            ts = datapoint.Timestep.Timestep()
+            ts.name = i * 180
+            ts.date = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(days=1, hours=i*3)
+
+            test_day_1.timesteps.append(ts)
         forecast.days.append(test_day_1)
+
+        # What is being asserted here?
+        #print(self.forecast.now())
+        assert forecast.now()
+
+    def test_at_datetime_1_5_hours_before_after(self):
+
+        target_before = datetime.datetime(2020, 3, 3, 7, 0,
+                                          tzinfo=datetime.timezone.utc)
+
+        target_after = datetime.datetime(2020, 3, 4, 23, 0,
+                                          tzinfo=datetime.timezone.utc)
+
+        self.assertRaises(datapoint.exceptions.APIException,
+                          self.forecast_3hrly.at_datetime, target_before)
+
+        self.assertRaises(datapoint.exceptions.APIException,
+                          self.forecast_3hrly.at_datetime, target_after)
+
+    def test_at_datetime_6_hours_before_after(self):
+
+        # Generate 2 timesteps These are set at 00:00 and 12:00
 
         target_before = datetime.datetime(2020, 3, 2, 15, 0,
                                           tzinfo=datetime.timezone.utc)
@@ -116,9 +108,28 @@ class TestForecast(unittest.TestCase):
         target_after = datetime.datetime(2020, 3, 6, 7, 0,
                                           tzinfo=datetime.timezone.utc)
 
+        self.assertRaises(datapoint.exceptions.APIException,
+                          self.forecast_daily.at_datetime, target_before)
 
         self.assertRaises(datapoint.exceptions.APIException,
-                          forecast.at_datetime, target_before)
+                          self.forecast_daily.at_datetime, target_after)
 
-        self.assertRaises(datapoint.exceptions.APIException,
-                          forecast.at_datetime, target_after)
+    def test_normal_time(self):
+        target = datetime.datetime(2020, 3, 3, 10, 0,
+                                          tzinfo=datetime.timezone.utc)
+
+        nearest = self.forecast_3hrly.at_datetime(target)
+        expected = datetime.datetime(2020, 3, 3, 9,
+                                     tzinfo=datetime.timezone.utc)
+        self.assertEqual(nearest.date, expected)
+
+    def test_forecase_midnight(self):
+        target = datetime.datetime(2020, 3, 4, 0, 15,
+                                          tzinfo=datetime.timezone.utc)
+
+        nearest = self.forecast_3hrly.at_datetime(target)
+        expected = datetime.datetime(2020, 3, 4, 0,
+                                     tzinfo=datetime.timezone.utc)
+        self.assertEqual(nearest.date, expected)
+
+
