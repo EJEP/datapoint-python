e700d768f1de5464a74bb7e72f70738bb0bb904e
Emlyn Price <emlyn.je.price@gmail.com>
2020-03-05 20:27:37 +0000

    Test raising of exception if 3hourly forecast and too early or late

--- tests/unit/test_forecast.py
+++ tests/unit/test_forecast.py
@@ -35,4 +35,50 @@ class TestForecast(unittest.TestCase):
         self.forecast.days.append(test_day_1)
 
         # What is being asserted here?
+        #print(self.forecast.now())
         assert self.forecast.now()
+
+    def test_at_datetime_1_5_hours_before_after(self):
+
+        # Generate 8 timesteps These are set at 00:00, 03:00, 06:00, 09:00,
+        # 12:00, 15:00, 18:00, 21:00 and don't got far into the past For this
+        # test we are checking the forecast for 07:00 on 2020-03-03, at 08:45
+        # on 2020-03-03 so we need timesteps from 09:00 (5 of them) and the day
+        # after Also checked is the forecast for 23:00 on 2020-03-04. These
+        # both raise APIException
+
+        test_day_0 = datapoint.Day.Day()
+        test_day_0.date = datetime.datetime(2020, 3, 3, tzinfo=datetime.timezone.utc)
+
+        for i in range(5):
+            ts = datapoint.Timestep.Timestep()
+            ts.name = i+9
+            ts.date = datetime.datetime(2020, 3, 3, 9+(3*i), tzinfo=datetime.timezone.utc)
+            test_day_0.timesteps.append(ts)
+
+        test_day_1 = datapoint.Day.Day()
+        test_day_1.date = datetime.datetime(2020, 3, 4, tzinfo=datetime.timezone.utc)
+
+        for i in range(8):
+            ts = datapoint.Timestep.Timestep()
+            ts.name = i+9
+            ts.date = datetime.datetime(2020, 3, 3, 3*i, tzinfo=datetime.timezone.utc)
+            test_day_1.timesteps.append(ts)
+
+        forecast = datapoint.Forecast.Forecast()
+
+        forecast.days.append(test_day_0)
+        forecast.days.append(test_day_1)
+
+        target_before = datetime.datetime(2020, 3, 3, 7, 0,
+                                          tzinfo=datetime.timezone.utc)
+
+        target_after = datetime.datetime(2020, 3, 4, 23, 0,
+                                          tzinfo=datetime.timezone.utc)
+
+
+        self.assertRaises(datapoint.exceptions.APIException,
+                          forecast.at_datetime, target_before)
+
+        self.assertRaises(datapoint.exceptions.APIException,
+                          forecast.at_datetime, target_after)
