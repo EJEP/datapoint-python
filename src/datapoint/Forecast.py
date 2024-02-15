794f394d895a7e448949ad0286aaa5b72bc97648
Emlyn Price <emlyn.je.price@gmail.com>
2024-02-15 20:33:55 +0000

    Begin adding new tests

--- src/datapoint/Forecast.py
+++ src/datapoint/Forecast.py
@@ -5,12 +5,34 @@ from datapoint.weather_codes import WEATHER_CODES
 
 
 class Forecast:
+    """Forecast data returned from DataPoint
+
+    Provides access to forecasts as far ahead as provided by DataPoint:
+    + x for hourly forecasts
+    + y for three-hourly forecasts
+    + z for daily forecasts
+
+    Basic Usage::
+
+    >>> import datapoint
+    >>> m = datapoint.Manager(api_key = "blah")
+    >>> f = m.get_forecast(latitude=50, longitude=0, frequency="hourly")
+    >>> f.now()
+    <TBD>
+    """
+
     def __init__(self, frequency, api_data):
+        """
+        :param frequency: Frequency of forecast: 'hourly', 'three-hourly' or 'daily'
+        :param api_data: Data returned from API call
+        :type frequency: string
+        :type api_data: dict
+        """
         self.frequency = frequency
         self.data_date = datetime.datetime.fromisoformat(
             api_data["features"][0]["properties"]["modelRunDate"]
         )
-        self.name = api_data["features"][0]["properties"]["location"]
+        self.name = api_data["features"][0]["properties"]["location"]["name"]
         self.forecast_longitude = api_data["features"][0]["geometry"]["coordinates"][0]
         self.forecast_latitude = api_data["features"][0]["geometry"]["coordinates"][1]
         self.distance_from_requested_location = api_data["features"][0]["properties"][
@@ -33,6 +55,19 @@ class Forecast:
                 self.timesteps.append(self.__build_timestep(forecast, parameters))
 
     def __build_timesteps_from_daily(self, forecasts, parameters):
+        """Build individual timesteps from forecasts and metadata
+
+        Take the forecast data from DataHub and combine with unit information
+        in each timestep. Break each day into day and night steps. ASSUME that
+        each step has data for the night referred to in the timestamp and the
+        following dawn-dusk period.
+
+        :parameter forecasts: Forecast data from DataHub
+        :parameter parameters: Unit information from DataHub
+
+        :return: List of timesteps
+        :rtype: list
+        """
         timesteps = []
         for forecast in forecasts:
             night_step = {"time": datetime.datetime.fromisoformat(forecast["time"])}
@@ -85,6 +120,19 @@ class Forecast:
         return timesteps
 
     def __build_timestep(self, forecast, parameters):
+        """Build individual timestep from forecast and metadata
+
+        Take the forecast data from DataHub for a single time and combine with
+        unit information in each timestep.
+
+        :parameter forecasts: Forecast data from DataHub
+        :parameter parameters: Unit information from DataHub
+
+        :return: Individual forecast timestep
+        :rtype: dict
+
+        """
+
         timestep = {}
         for element, value in forecast.items():
             if element == "time":
@@ -107,6 +155,11 @@ class Forecast:
         return timestep
 
     def __check_requested_time(self, target):
+        """Check that a forecast for the requested time can be provided
+
+        :parameter target: The requested time for the forecast
+        :type target: datetime
+        """
         # Check that there is a forecast for the requested time.
         # If we have an hourly forecast, check that the requested time is at
         # most 30 minutes before the first datetime we have a forecast for.
@@ -170,7 +223,15 @@ class Forecast:
             raise APIException(err_str)
 
     def at_datetime(self, target):
-        """Return the timestep closest to the target datetime"""
+        """Return the timestep closest to the target datetime
+
+        :parameter target: Time to get the forecast for
+        :type target: datetime
+
+        :return: Individual forecast timestep
+        :rtype: dict
+
+        """
 
         # Convert target to offset aware datetime
         if target.tzinfo is None:
@@ -204,17 +265,48 @@ class Forecast:
         return to_return
 
     def now(self):
-        """Function to return the closest timestep to the current time"""
+        """Function to return the closest timestep to the current time
+
+        :return: Individual forecast timestep
+        :rtype: dict
+        """
 
         d = datetime.datetime.now(tz=self.timesteps[0]["time"].tzinfo)
         return self.at_datetime(d)
 
-    def future(self, in_days=0, in_hours=0, in_minutes=0, in_seconds=0):
-        """Return the closest timestep to a date in a given amount of time"""
+    def future(self, days=0, hours=0, minutes=0):
+        """Return the closest timestep to a date in a given amount of time
+
+        Either provide components of the time to the forecast or the total
+        hours or minutes
+
+        Providing components::
+
+        >>> import datapoint
+        >>> m = datapoint.Manager(api_key = "blah")
+        >>> f = m.get_forecast(latitude=50, longitude=0, frequency="hourly")
+        >>> f.future(days=1, hours=2)
+
+        Providing total hours::
+
+        >>> import datapoint
+        >>> m = datapoint.Manager(api_key = "blah")
+        >>> f = m.get_forecast(latitude=50, longitude=0, frequency="hourly")
+        >>> f.future(hours=26)
+
+
+        :parameter days: How many days ahead
+        :parameter hours: How many hours ahead
+        :parameter minutes: How many minutes ahead
+        :type days: int
+        :type hours: int
+        :type minutes: int
+
+        :return: Individual forecast timestep
+        :rtype: dict
+        """
 
         d = datetime.datetime.now(tz=self.timesteps[0]["time"].tzinfo)
-        target = d + datetime.timedelta(
-            days=in_days, hours=in_hours, minutes=in_minutes, seconds=in_seconds
-        )
+        target = d + datetime.timedelta(days=days, hours=hours, minutes=minutes)
 
         return self.at_datetime(target)
--- src/datapoint/Manager.py
+++ src/datapoint/Manager.py
@@ -11,21 +11,26 @@ from datapoint.exceptions import APIException
 from datapoint.Forecast import Forecast
 
 API_URL = "https://data.hub.api.metoffice.gov.uk/sitespecific/v0/point/"
-DATE_FORMAT = "%Y-%m-%dZ"
-DATA_DATE_FORMAT = "%Y-%m-%dT%XZ"
-FORECAST_DATE_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 
 
 class Manager:
     """
     Datapoint Manager object
+
+    Wraps calls to DataHub API, and provides Forecast objects
+    Basic Usage::
+
+    >>> import datapoint
+    >>> m = datapoint.Manager(api_key = "blah")
+    >>> f = m.get_forecast(latitude=50, longitude=0, frequency="hourly")
+    >>> f.now()
+    <TBD>
     """
 
     def __init__(self, api_key=""):
         self.api_key = api_key
-        self.call_response = None
 
-    def __retry_session(
+    def __get_retry_session(
         self,
         retries=10,
         backoff_factor=0.3,
@@ -34,7 +39,17 @@ class Manager:
     ):
         """
         Retry the connection using requests if it fails. Use this as a wrapper
-        to request from datapoint
+        to request from datapoint. See
+        https://requests.readthedocs.io/en/latest/user/advanced/?highlight=retry#example-automatic-retries
+        for more details.
+
+        :parameter retries: How many times to retry
+        :parameter backoff_factor: Backoff between attempts after second try
+        :parameter status_forcelist: Codes to force a retry on
+        :parameter session: Existing session to use
+
+        :return: Session object
+        :rtype: TBD
         """
 
         # requests.Session allows finer control, which is needed to use the
@@ -61,6 +76,15 @@ class Manager:
         """
         Call the datapoint api using the requests module
 
+        :parameter latitude: Latitude of forecast location
+        :parameter longitude: Longitude of forecast location
+        :parameter frequency: Forecast frequency. One of 'hourly', 'three-hourly, 'daily'
+        :type latitude: float
+        :type longitude: float
+        :type frequency: string
+
+        :return: Data from DataPoint
+        :rtype: dict
         """
         params = {
             "latitude": latitude,
@@ -83,7 +107,10 @@ class Manager:
         # object. This has a .get() function like requests.get(), so the use
         # doesn't change here.
 
-        sess = self.__retry_session()
+        print(request_url)
+        print(params)
+        print(headers)
+        sess = self.__get_retry_session()
         req = sess.get(
             request_url,
             params=params,
@@ -91,59 +118,36 @@ class Manager:
             timeout=1,
         )
 
+        req.raise_for_status()
+
         try:
             data = geojson.loads(req.text)
         except ValueError as exc:
-            raise APIException(
-                "DataPoint has not returned any data, this could be due to an incorrect API key"
-            ) from exc
-        self.call_response = data
-        if req.status_code != 200:
-            msg = [
-                data[m] for m in ("message", "error_message", "status") if m in data
-            ][0]
-            raise Exception(msg)
+            raise APIException("DataPoint has not returned valid JSON") from exc
+
         return data
-
-    def _visibility_to_text(self, distance):
-        """
-        Convert observed visibility in metres to text used in forecast
-        """
-
-        if not isinstance(distance, int):
-            raise ValueError("Distance must be an integer not", type(distance))
-        if distance < 0:
-            raise ValueError("Distance out of bounds, should be 0 or greater")
-
-        if 0 <= distance < 1000:
-            return "VP"
-        elif 1000 <= distance < 4000:
-            return "PO"
-        elif 4000 <= distance < 10000:
-            return "MO"
-        elif 10000 <= distance < 20000:
-            return "GO"
-        elif 20000 <= distance < 40000:
-            return "VG"
-        else:
-            return "EX"
 
     def get_forecast(self, latitude, longitude, frequency="daily"):
         """
         Get a forecast for the provided site
 
-        A frequency of "daily" will return two timesteps:
-        "Day" and "Night".
+        :parameter latitude: Latitude of forecast location
+        :parameter longitude: Longitude of forecast location
+        :parameter frequency: Forecast frequency. One of 'hourly', 'three-hourly, 'daily'
+        :type latitude: float
+        :type longitude: float
+        :type frequency: string
 
-        A frequency of "3hourly" will return 8 timesteps:
-        0, 180, 360 ... 1260 (minutes since midnight UTC)
+        :return: :class: `Forecast <Forecast>` object
+        :rtype: datapoint.Forecast
         """
         if frequency not in ["hourly", "three-hourly", "daily"]:
             raise ValueError(
                 "frequency must be set to one of 'hourly', 'three-hourly', 'daily'"
             )
         data = self.__call_api(latitude, longitude, frequency)
-        # print(data)
+        #with open('./hourly_api_data.json', 'w') as f:
+        #    geojson.dump(data, f)
         forecast = Forecast(frequency=frequency, api_data=data)
 
         return forecast
